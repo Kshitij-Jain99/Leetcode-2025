59a312042eac3b6847766301ece8670958bc40f6
KAIN <kshitijjain6074@gmail.com>
2025-01-13 23:47:36 +0530

    3223. Minimum Length of String After Operations

--- DCC January/Jan13_A.java
+++ DCC January/Jan13_A.java
@@ -7,21 +7,21 @@
  3. When freq = 0/1/2  -> no change
  */
 
-public class Jan13_A {
-    public int minLength(String s){
-    int[] freq = new int[26]; //to store freq of characters
-    for(char c: s.toCharArray()){
-        freq[c - 'a']++; //increment frq of char
-    }
-
-    int length = 0;
-    for(int count: freq) {
-        if(count%2 == 1) { //odd freq
-            length += 1; //contribution in len
-        } else {
-            length += Math.min(2,count); //even freq, contribution in len
+ public class Jan13_A {
+    public int minLength(String s) {
+        int[] freq = new int[26]; // to store frequencies of characters
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++; // increment frequency of the character
         }
+
+        int length = 0;
+        for (int count : freq) {
+            if (count % 2 == 1) { 
+                length += 1; // odd frequency contributes 1
+            } else {
+                length += Math.min(2, count); // even frequency contributes fully
+            }
+        }
+        return length;
     }
-    return length;
- }
 }
