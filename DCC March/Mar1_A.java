6a0509c7687d803f292b34f8f662c5b2477aa633
KAIN <kshitijjain6074@gmail.com>
2025-03-01 12:38:35 +0530

    //2460. Apply Operations to an Array

---
+++ DCC March/Mar1_A.java
@@ -0,0 +1,25 @@
+//2460. Apply Operations to an Array
+// Appraoch A: Naive Appraoch: Extra array used
+// TC = O(N), SC = O(N)
+public class Mar1_A {
+    public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+        for(int i =0; i<n-1; i++){
+            if(nums[i] == nums[i+1]){
+                nums[i] *= 2;
+                nums[i+1] = 0;
+            }
+          
+        }
+        int[] res = new int[n];
+        int idx = 0;  //to track pos where next non-zero no. should be placed
+
+        for(int num : nums){
+            if(num != 0){
+                res[idx++] = num;
+            }
+        }
+        return res;
+    }
+
+}
---
+++ DCC March/Mar1_B.java
@@ -0,0 +1,29 @@
+//2460. Apply Operations to an Array
+// Appraoch B: Optimized Two pointers: In-place
+// TC = O(N), SC = O(1)
+
+public class Mar1_B {
+    public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+        for(int i =0; i<n-1; i++){
+            if(nums[i] == nums[i+1]){
+                nums[i] *= 2;
+                nums[i+1] = 0;
+            }
+          
+        }
+       
+        int idx = 0;  //to track pos where next non-zero no. should be placed
+        
+        for(int i=0; i<n; i++){
+            if(nums[i] != 0){
+                nums[idx++] = nums[i];
+            }
+        }
+        //fill rem pos with 0
+        while(idx<n){
+            nums[idx++] = 0;
+        }
+        return nums;
+    }
+}
