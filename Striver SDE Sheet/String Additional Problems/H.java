8b546e80c07b179971ca68d0fa5e5bad9e2afe7e
KAIN <kshitijjain6074@gmail.com>
2025-10-13 20:32:29 +0530

    3713, 3714. Longest Balanced Substring I, II

---
+++ Striver SDE Sheet/String Additional Problems/H.java
@@ -0,0 +1,12 @@
+//3713. Longest Balanced Substring I: https://leetcode.com/problems/longest-balanced-substring-i/description/
+//3714. Longest Balanced Substring II: https://leetcode.com/problems/longest-balanced-substring-ii/description/
+
+//Longest Balanced Substring I:
+public class H{
+    
+    //Approach-1: 
+    // TC = O(), SC = O()
+    
+
+
+}
