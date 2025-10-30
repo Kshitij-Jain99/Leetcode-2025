47d889e3ef8641fabedea115b0dd395f5a595bfa
Kshitij Jain <kshitijjain6074@gmail.com>
2025-10-31 03:29:22 +0530

    3715. Sum of Perfect Square Ancestors:

--- Random Problems/K.java
+++ Random Problems/K.java
@@ -1,4 +1,7 @@
-//3715. Sum of Perfect Square Ancestors: https://leetcode.com/problems/sum-of-perfect-square-ancestors/description/
+
 public class K{
     
+    //Approach-1:
+    //TC = O(), SC = O()
+
 }
